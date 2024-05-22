43f41a7ada72cc39c2e82489876cd24705662d4f
swlodawski <109826027+swlodawski@users.noreply.github.com>
2024-05-22 19:13:00 -0400

    Adding API to test functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,10 @@
-var limit = 3
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/dadjokes?limit=' + limit,
-    headers: { 'X-Api-Key': 'Xu8l5GP3DoNWZttwPGNDxQ==PdUVGzeb0vkzwS7h'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+fetch('https://api.chucknorris.io/jokes/random'
+  // The browser fetches the resource from the remote server without first looking in the cache.
+  // The browser will then update the cache with the downloaded resource.
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });

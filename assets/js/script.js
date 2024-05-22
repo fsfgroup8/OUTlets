f480e279780b1740707a16093cd703f37ec5a08d
fsfgroup8 <fsfgroup8@gmail.com>
2024-05-22 19:40:51 -0400

    Update script.js
    Removed old api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,1 @@
-var limit = 3
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/dadjokes?limit=' + limit,
-    headers: { 'X-Api-Key': 'Xu8l5GP3DoNWZttwPGNDxQ==PdUVGzeb0vkzwS7h'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
+

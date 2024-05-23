c73ef0c0c3829c740bb850d8207327562ac15095
swlodawski <109826027+swlodawski@users.noreply.github.com>
2024-05-23 12:52:46 -0400

    Moving js from js2 file to js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,33 @@
+fetch('https://api.chucknorris.io/jokes/random', {
+  // The browser fetches the resource from the remote server.
+  // The browser will then update new created element jokeEl with the downloaded resource.
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+
+
+  const createRandomJoke = function(data) {
+    const jokeContainer = document.createElement('div')
+    const jokeEl = document.createElement('p');
+// Add the values to snatch from the API object to deliver random joke will go below this comment.
+    jokeEl.textContent.categories[0].value('');
+    jokeEl.setAttribute('style', 'color: black');
+
+    jokeContainer.append(jokeEl);
+
+  }
+
+  createRandomJoke();
+
+
+
 const searchBtn = document.getElementById('search button');
 
 document.addEventListener('click', function() {
-
+// Event listener for html landing page search button.
 })

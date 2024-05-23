159d4322b8868eebe115ed9e7e067d5fc1bddfbe
swlodawski <109826027+swlodawski@users.noreply.github.com>
2024-05-23 12:26:21 -0400

    Added comment to API request pull section.

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,6 +1,6 @@
 fetch('https://api.chucknorris.io/jokes/random', {
-  // The browser fetches the resource from the remote server without first looking in the cache.
-  // The browser will then update the cache with the downloaded resource.
+  // The browser fetches the resource from the remote server.
+  // The browser will then update new created element jokeEl with the downloaded resource.
 })
   .then(function (response) {
     return response.json();

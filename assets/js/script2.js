14890d9e6b626e259e208167b9b9d62a3f68a732
swlodawski <109826027+swlodawski@users.noreply.github.com>
2024-05-24 13:30:12 -0400

    Adding substance to the location function and event function.

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -10,9 +10,9 @@
 // ----------------- Functions section below -----------------
 
 
-fetch('https://api.positionstack.com/v1/forward? access_key = ec3c837ab8d9eaa67a161607890ea491', {
+fetch('https://api.positionstack.com/v1/forward? access_key=ec3c837ab8d9eaa67a161607890ea491', {
   // The browser fetches the resource from the remote server.
-  // The browser will then update new created element jokeEl with the downloaded resource.
+  // The browser will find the users location based on a location identifier(i.e city, state, long, lat or IP Address).
 })
   .then(function (response) {
     return response.json();
@@ -20,6 +20,29 @@ fetch('https://api.positionstack.com/v1/forward? access_key = ec3c837ab8d9eaa67a
   .then(function (data1) {
     console.log(data1);
   });
+
+  
+  const createUserLocation = function(data1) {
+    const userLocationContainer = document.createElement('div')
+    const userLocationTitle = document.createElement('h3')
+    const userLocationCity = document.createElement('p')
+    // Maybe const userLongitude = document.createElement('p')
+    // Maybe const userLatitude = document.createElement('p')  
+
+//Put the value pull from the location API here.
+    userLocationTitle
+    userLocationCity
+    // userLongitude
+    // userLatitude
+
+// Set attribute section for all elements inside the creatuserlocation container.
+    userLocationContainer.append(userLocationTitle, userLocationCity, userEventSuggestion)
+
+  }
+
+  createUserLocation();
+// Calling User Location Function
+
 
   
   fetch('GET https://api.predicthq.com/v1/events HTTP/1.1 Accept: application/json Authorization: dxKKKuK5-dI $dzguvnsi5sFnOMIG7JAnu6MqNZ__5XmXJjhkxa2QRBCxFAAP7fVTXg', {
@@ -34,41 +57,31 @@ fetch('https://api.positionstack.com/v1/forward? access_key = ec3c837ab8d9eaa67a
       console.log(data2);
     });
 
-  const createUserLocation = function(data2) {
-    const userLocationContainer = document.createElement('div')
-    const userLocationTitle = document.createElement('h3')
-    const userLocationCity = document.createElement('h4')
-    const userEventSuggestion = document.createElement('h4')
+    const createUserEvent = function(data2) {
+        const eventContainer = document.createElement('div');
+        const eventTitle = document.createElement('h2');
+        const eventType = document.createElement('h4');
+
 //Put the value pull from the location API here.
-    userLocationTitle
-    userLocationCity
-    userEventSuggestion
+        eventTitle
+        eventType
 
-// Set attribute section for all elements inside the creatuserlocation container.
-    userLocationContainer.append(userLocationTitle, userLocationCity, userEventSuggestion)
+// Appending the grapped values to the event container div.
+        eventContainer.append(eventTitle, eventTitle);
+    }
 
-  }
-
-  createUserLocation();
-
-
-  const createUserEvent = function() {
-
-  }
-
-  createUserEvent();
-//   This will be the function that creates the user event suggestions based on the user's interests and location.
+    createUserEvent();
+// This function will fectch and event an display it based on ther users interests.
 
 
-
-
-// Below will be a functon to merge the two data points and output the combined result.
 
 const createEventSuggestion = function(data1, data2) {
 
 }
+  //   This will be the function that creates the user event suggestions based on the user's interests and location.
 
 createEventSuggestion();
+// This will call the create user event function.
 
 
 

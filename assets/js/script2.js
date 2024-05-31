bbdd972ec6af132db7bbaf85ab6d1d6818543715
AA <aalborgil002@gmail.com>
2024-05-31 14:08:01 -0400

    Added localStorage Function in JS-1File

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,43 +1,43 @@
-// // ----------------- Global variables section below -----------------
+// ----------------- Global variables section below -----------------
 
-const url = 'https://api.seatgeek.com/2/venues?city={this will grab form value from hobby list 1 value}'
-const apiKey = 'efd618d723ac9af1b3cb310dd19590338cd98b5bd32204a2e633c7bdcfb2f5f9'
-const clientId = 'NDE4MTc1OTZ8MTcxNjY2OTE4NC43MTg5Nw'
-const events = 'events'
-const qURL = `${url}/${events}?client_id=${clientId}`
+const cityName = localStorage.getItem('citySelection');
+const eventName = localStorage.getItem('eventSelection');
+const clientId = 'NDE4MTc1OTZ8MTcxNjY2OTE4NC43MTg5Nw';
+const apiKey = 'efd618d723ac9af1b3cb310dd19590338cd98b5bd32204a2e633c7bdcfb2f5f9';
 
-const headers = {
-  'X-API-Key': apiKey,
-};
+// Check if cityName is available to construct the URL
+if (cityName) {
+  const url = `https://api.seatgeek.com/2/venues?city=${cityName}&client_id=${clientId}`;
 
-const options = {
-  method: 'GET',
-  headers: headers,
-};
+  const headers = {
+    'X-API-Key': apiKey,
+  };
 
+  const options = {
+    method: 'GET',
+    headers: headers,
+  };
 
-//   // ----------------- Functions section below -----------------
+  // ----------------- Functions section below -----------------
 
-fetch(qURL, options)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+  fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+} else {
+  console.error('City name is not available in localStorage.');
+}
 
-
-// // // ----------------- Event listener section below-----------------
-
-
-
-
+// ----------------- Event listener section below -----------------
 
 
 

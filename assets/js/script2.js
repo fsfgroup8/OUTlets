e463e7c273475950d20ea0c68ba6ca6661e26caf
Al Bundy <147283600+galessalazar@users.noreply.github.com>
2024-05-30 23:44:35 -0500

    changed the array from 10 events to 100

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,24 +1,30 @@
 // // ----------------- Global variables section below -----------------
 
-const url = 'https://api.seatgeek.com/2/venues?city={this will grab form value from hobby list 1 value}'
+const url = 'https://api.seatgeek.com/2'
 const apiKey = 'efd618d723ac9af1b3cb310dd19590338cd98b5bd32204a2e633c7bdcfb2f5f9'
 const clientId = 'NDE4MTc1OTZ8MTcxNjY2OTE4NC43MTg5Nw'
 const events = 'events'
-const qURL = `${url}/${events}?client_id=${clientId}`
+const perPage = '200'
+const venues = 'venues'
+const qURL = `${url}/${events}?per_page=${perPage}&client_id=${clientId}`
 
-const headers = {
-  'X-API-Key': apiKey,
-};
+// cant seem to use venues without their unique IDs
 
-const options = {
-  method: 'GET',
-  headers: headers,
-};
+console.log(qURL);
+
+// const headers = {
+//   'X-API-Key': apiKey,
+// };
+
+// const options = {
+//   method: 'GET',
+//   headers: headers,
+// };
 
 
 //   // ----------------- Functions section below -----------------
-
-fetch(qURL, options)
+const options = 'options'
+fetch(qURL)
   .then(response => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
